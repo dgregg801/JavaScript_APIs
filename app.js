685ad2aea3bc2f28f351395f0870ccf251d20fcd
Darin Gregg <daringregg801@gmail.com>
2023-09-26 08:40:16 -0600

    exercise complete

--- app.js
+++ app.js
@@ -2,3 +2,36 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const UrlAddress = "https://api.giphy.com/v1/gifs/translate";
+const apiKey = "dDiB2TbsVlgYtU3itSJwjhR5dM6PUZXj";
+
+let searchInput = document.querySelector("#searchWord");
+let searchBtn = document.querySelector("#submitSearch");
+let gif = document.querySelector("#imageContainer > img");
+let feedback = document.querySelector("#feedback");
+
+console.log(feedback);
+console.log(searchInput);
+
+//Exercise 2
+const searchTerm = searchInput.value;
+const url = `data.images.original.url`;
+searchBtn.addEventListener("click", (event) => {
+  fetch(`${UrlAddress}?api_key=${apiKey}&s=${searchInput.value}`)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+
+      if (Array.isArray(result.data) && result.data.length <= 0) {
+        feedback.textContent = "No gifs found";
+      } else {
+        gif.src = result.data.images.original.url;
+        searchInput.value = "";
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      feedback.textContent = error.message;
+    });
+});
